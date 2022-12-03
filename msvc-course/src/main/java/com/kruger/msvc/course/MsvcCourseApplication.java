5e5362fb154a9c8b8e34f9305fec4e34d01b8438
Jonathan <jonato96s@gmail.com>
2022-12-03 16:47:32 -0500

    implement two mcsvs

---
+++ msvc-course/src/main/java/com/kruger/msvc/course/MsvcCourseApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.msvc.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCourseApplication.class, args);
+	}
+
+}
